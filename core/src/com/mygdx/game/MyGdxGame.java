ba29f5c7c745a647c0e58d3b0c5de87162d4ef01
jonathandavidblack <jonnybl915@gmail.com>
2016-06-02 22:18:15 -0400

    finished running animation with minor bugs in left run function

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -11,7 +11,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+
 	TextureRegion down;
 	TextureRegion downFlip;
 	TextureRegion up;
@@ -20,16 +20,33 @@ public class MyGdxGame extends ApplicationAdapter {
 	TextureRegion left;
 	TextureRegion standingR;
 	TextureRegion standingL;
+	TextureRegion huStanding;
+	TextureRegion huStandingL;
+	TextureRegion huDown;
+	TextureRegion huDownFlip;
+	TextureRegion huRight;
+	TextureRegion huLeft;
+	TextureRegion huUp;
+	TextureRegion huUpFlip;
+
+
 	Animation walkRight;
 	Animation walkLeft;
 	Animation walkUp;
 	Animation walkDown;
+	Animation runDown;
+	Animation runRight;
+	Animation runLeft;
+	Animation runUp;
+
+
 	float time;
 	int movementMem;
 	float x, y, xv, yv;
+	int runMem;
 
 	static final float MAX_VELOCITY = 100;
-	static final float DECELERATOR = 0.8f;
+	static final float DECELERATOR = 0.75f;
 	static final int WIDTH = 18;
 	static final int HEIGHT = 22;
 	
@@ -52,12 +69,26 @@ public class MyGdxGame extends ApplicationAdapter {
 		standingR = grid[6][2];
 		standingL = new TextureRegion(standingR);
 		standingL.flip(true, false);
+		huStanding = grid [7][2];
+		huStandingL = new TextureRegion(huStanding);
+		huStandingL.flip(true, false);
+		huRight = grid [7][3];
+		huUp = grid[7][1];
+		huUpFlip = new TextureRegion(huUp);
+		huUpFlip.flip(true, false);
+		huDown = grid[7][0];
+		huDownFlip = new TextureRegion(huDown);
+		huDownFlip.flip(true, false);
 
 
 		walkRight = new Animation(0.2f, grid[6][3], grid[6][2]);
 		walkLeft = new Animation(0.2f, left, standingL);
 		walkUp = new Animation(0.2f, up, upFlip);
 		walkDown = new Animation(0.2f, down, downFlip);
+		runRight = new Animation(0.2f, huRight, huStanding);
+		runLeft = new Animation(0.2f, huLeft, huStandingL);
+		runUp = new Animation(0.2f, huUp, huUpFlip);
+		runDown = new Animation(0.2f, huDown, huDownFlip);
 	}
 
 	@Override
@@ -67,16 +98,29 @@ public class MyGdxGame extends ApplicationAdapter {
 
 		if(xv>0){
 			right = walkRight.getKeyFrame(time, true);
+			if(xv > MAX_VELOCITY){
+				right = runRight.getKeyFrame(time, true);
+			}
 		}
 		if(xv<0){
 			left = (walkLeft.getKeyFrame(time, true));
+			if(xv < -MAX_VELOCITY){							//**** needs some work.
+				left = runLeft.getKeyFrame(time, false);	//******
+			}
 		}
 		if(yv>0){
 			up = (walkUp.getKeyFrame(time, true));
+			if(yv > MAX_VELOCITY){
+				up = runUp.getKeyFrame(time, true);
+			}
 		}
 		if(yv<0){
 			down = (walkDown.getKeyFrame(time, true));
+			if(yv < -MAX_VELOCITY){
+				down = runDown.getKeyFrame(time, true);
+			}
 		}
+
 
 		Gdx.gl.glClearColor(.2f, 1, .7f, .5f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -98,12 +142,15 @@ public class MyGdxGame extends ApplicationAdapter {
 			tempImg = up;
 		}
 		else{
-			tempImg = right;
+			tempImg = left;
 		}
+
 		batch.draw(tempImg, x, y, WIDTH*2, HEIGHT*2);
 		batch.end();
 	}
+
 	public void move(){
+
 		float velocityBoost = MAX_VELOCITY + MAX_VELOCITY;
 
 		if (Gdx.input.isKeyPressed(Input.Keys.UP)){
@@ -112,7 +159,6 @@ public class MyGdxGame extends ApplicationAdapter {
 				yv = velocityBoost;
 			}
 			movementMem = 1;
-
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
 			yv = -MAX_VELOCITY;
@@ -127,7 +173,6 @@ public class MyGdxGame extends ApplicationAdapter {
 				xv = velocityBoost;
 			}
 			movementMem = 3;
-
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
 			xv = -MAX_VELOCITY;
@@ -136,6 +181,8 @@ public class MyGdxGame extends ApplicationAdapter {
 			}
 			movementMem = 4;
 		}
+
+
 
 		if(x>Gdx.graphics.getWidth()){
 			x = -5;
@@ -155,6 +202,8 @@ public class MyGdxGame extends ApplicationAdapter {
 		yv = decelerate(yv);
 		xv = decelerate(xv);
 	}
+
+
 	public float decelerate(float velocity){
 		velocity *= DECELERATOR;
 		if (Math.abs(velocity) <1) {
